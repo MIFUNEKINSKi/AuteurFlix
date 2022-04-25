14aeb81a95878dc586ae693d2a852fb8ecc9e796
Full Name <email@address.com>
2022-04-25 16:00:52 -0400

    started user auth

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,12 +2,15 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
-
+import App from './app';
 
 
 const Root = ({ store }) => (
-
-        <h1>This is A test</h1>
+  <Provider store={store}>
+    <HashRouter>
+      <App store={store} />
+    </HashRouter>
+  </Provider>
 );
 
 export default Root;
