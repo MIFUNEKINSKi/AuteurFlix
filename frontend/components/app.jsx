26faf5c61f52eb67cbf9ace900c77a66be068e18
Full Name <email@address.com>
2022-04-25 15:41:03 -0400

    fixed action controller bug

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -8,7 +8,6 @@ const App = () => (
   <div>
     <Switch>
       <AuthRoute exact path='/' component={Splash} />
-
     </Switch>
 
   </div>
---
+++ frontend/components/splash/signup_footer.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => (
+  <div className="splash-footer">
+    <a href="https://github.com/MifuneKinski'">Github</a>
+    <a href="https://github.com/MifuneKinski'">Linked In</a>
+    <a href="mailto:moorecash@gmail.com">Email</a>
+  </div>
+)
