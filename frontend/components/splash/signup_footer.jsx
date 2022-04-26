3ad1c80f31cf8e24c4bbefa1a7b685ecf066bf46
Chris <moorecash@gmail.com>
2022-04-26 15:50:39 -0400

    fixed links

--- frontend/components/splash/signup_footer.jsx
+++ frontend/components/splash/signup_footer.jsx
@@ -2,8 +2,8 @@ import React from "react"
 
 export default () => (
   <div className="splash-footer">
-    <a href="https://github.com/MifuneKinski'">Github</a>
-    <a href="https://github.com/MifuneKinski'">Linked In</a>
+    <a href="https://github.com/MifuneKinski">Github</a>
+    <a href="https://github.com/MifuneKinski">LinkedIn</a>
     <a href="mailto:moorecash@gmail.com">Email</a>
   </div>
 )
