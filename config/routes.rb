3c5fec7020d3c068d62eaf0b9dcded71efd88665
Chris <moorecash@gmail.com>
2022-04-26 11:57:25 -0400

    fixed npm with Jacob

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
   namespace :api, defaults: {format: :json} do 
-    resources :users, only: [:create, :show]
-  end 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+    resources :profiles, only: [:show, :update, :destroy]
+    resource :session, only: [:create, :destroy]
+    resources :users, only: [:create, :show] do 
+      resources :profiles, only: [:index, :create]
+    end 
+  end
 end
--- frontend/components/splash/splash_header.jsx
+++ frontend/components/splash/splash_header.jsx
@@ -2,7 +2,10 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 export default () => (
-  <div className="splash-header">
-      <Link className="signin-btn" to='/login'>Log In</Link> 
-  </div>
+ 
+ <div > 
+ <h1>Log In</h1>
+  {/* <Link className="signin-btn" to='/login'>Log In</Link>  */}
+</div>
+
 );
