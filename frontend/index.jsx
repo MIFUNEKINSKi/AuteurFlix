dfe6fed29bff179685306ad34456dad3fc4972c8
Chris <moorecash@gmail.com>
2022-05-06 07:42:04 -0400

    heroku change

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import "core-js/stable";
+// import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 document.addEventListener('DOMContentLoaded', () => {
