e479884e4dcf07bec3cb67bfcaf0be86f3b5570b
Chris <moorecash@gmail.com>
2022-10-26 10:00:00 -0400

    aint easy being green

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -23,5 +23,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={init.store} persistor={init.persistor} />, root);
+  // 
 
 });
