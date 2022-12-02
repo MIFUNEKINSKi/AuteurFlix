3700fc467a93ddb64fc60bea1166099379eedd83
Chris <moorecash@gmail.com>
2022-12-02 10:32:10 -0500

    test

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={init.store} persistor={init.persistor} />, root);
-  // green
+  // gree
 
 });
 
